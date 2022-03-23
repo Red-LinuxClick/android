5745a9b16f0262d998369b6bb9197d3c52aaba4f
Angel Romero <cromstudioin@gmail.com>
2022-03-22 20:50:58 -0430

    Beta 1.0

---
+++ app/src/main/java/com/cromstudio/redlinuxclick/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.cromstudio.redlinuxclick
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient;
+import android.webkit.WebResourceRequest;
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    lateinit var mAdView : AdView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+        val myWebView = findViewById<WebView>(R.id.myWebView)
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?): Boolean {
+                return false
+            }
+        }
+        myWebView.settings.javaScriptEnabled = true
+
+        myWebView.loadUrl("https://redlinuxclick.ml")
+
+
+    }
+
+    }
+
+
+
+
+
